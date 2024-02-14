40a16c16eb39e4ccd244c3ae090a69fd10a39c91
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-02-13 23:41:04 -0500

    useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   const [users, setUsers] = useState(null);
 
+  const fetchData = useCallback(async () => {
+    console.log("fetchData");
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (!response.ok) {
+        throw "Error al conectar la API";
+      }
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
+      setUsers([]);
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   console.log("useEffect");
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setUsers(data);
+  //     });
+  // }, []);
+
   useEffect(() => {
     console.log("useEffect");
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data);
-      });
-  }, []);
+    fetchData();
+  });
 
   if (!users) return <div>Cargando...</div>;
 
